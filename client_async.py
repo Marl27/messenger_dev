c633057ea76ea16c41f1e01c282d8030e62cf2b4
cruthe93 <cruthe93@gmail.com>
2022-09-28 16:40:11 +0100

    Update client_async.py
    Refactored client program to use OOP, can now generate as many as we want as instances for testing

--- client_async.py
+++ client_async.py
@@ -1,32 +1,45 @@
 import asyncio
+import random
+import time
 
-HOST = "127.0.0.1"
+HOSTNAME = "127.0.0.1"
 PORT = 8888
 
 
-async def tcp_echo_client(message: str):
-    """
-    Skeleton of a client program
-    Interestingly, asyncio open_connection defaults to IPv6
-    :param message: String to send
-    :return:
-    """
+class Client:
 
-    # will change this to be command line input
-    # either in a run loop after starting client program or from command line
-    reader, writer = await asyncio.open_connection(HOST, PORT)
-    print(f"Send: {message!r}")
-    writer.write(message.encode())
-    await writer.drain()
+    def __init__(self, hostname: str = HOSTNAME, port: int = PORT, name:str = "default_client"):
+        self.hostname = hostname
+        self.port = port
+        self.name = name
 
-    data = await reader.read(100)
-    print(f"Received: {data.decode()!r}")
+    async def tcp_echo_client(self, message: str):
+        """
+        Skeleton of a client program
+        Interestingly, asyncio open_connection defaults to IPv6
+        :param message: String to send
+        :return:
+        """
+
+        # will change this to be command line input
+        # either in a run loop after starting client program or from command line
+        reader, writer = await asyncio.open_connection(self.hostname, self.port)
+        print(f"Send: {message!r}")
+        writer.write(message.encode())
+        await writer.drain()
+
+        data = await reader.read(100)
+        print(f"Received: {data.decode()!r}")
+
+        print("Close the connection")
+        writer.close()
+        await writer.wait_closed()
 
 
-# print("Close the connection")
-# writer.close()
-# await writer.wait_closed()
+num_clients = 500
+clients = [Client(name=str(random.randint(0, 100))) for x in range(num_clients)]
 
-# Not sure if this is the correct way to loop on asyncio but it works
 while True:
-    asyncio.run(tcp_echo_client(input(">")), debug=True)
+    for client in clients:
+        asyncio.run(client.tcp_echo_client(client.name), debug=True)
+    time.sleep(5)
