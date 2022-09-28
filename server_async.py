298c34ee799b407e715e60934746f1d040f821ae
cruthe93 <cruthe93@gmail.com>
2022-09-28 16:12:55 +0100

    Update server_async.py
    Refactored server to use OOP

--- server_async.py
+++ server_async.py
@@ -4,53 +4,62 @@ HOSTNAME = "localhost"  # ip: 127.0.0.1
 PORT = 8888  # arbitrary high level port
 
 
-# Async def makes the function a 'coroutine'
-# basically a function that can be run using concurrency
-async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    """
-    Coroutine which handles incoming client connections parallel
-    :param reader: asyncio.StreamReader - wrapper for async reading to TCP sockets
-    :param writer: asyncio.StreamWriter - wrapper for async writing to TCP sockets
-    :return:
-    """
-    data = await reader.read(100)  # to run coroutines you need to call them using the 'await' keyword
-    message = data.decode()  # Decoding message from bytestream to utf-8 encoded text
-    addr = writer.get_extra_info("peername")
+class Server:
 
-    # TODO: change to use python logging module rather than print statements
-    print(f"Received {message!r} from {addr!r}")
-    print(f"Send: {message!r}")
+    def __init__(self):
+        self.connected_clients = []  # List for now, might need to change data structure
+        self.database = None  # Placeholder for database
+        self.hostname = HOSTNAME
+        self.port = PORT
 
-    # Need to use write with drain as it might be queued in a write buffer
-    # if it cannot be sent immediately
-    writer.write(data)
-    await writer.drain()
+        # Async def makes the function a 'coroutine'
 
-    # print("Close the connection")
-    writer.close()
+    # basically a function that can be run using concurrency
+    async def handle_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        """
+        Coroutine which handles incoming client connections parallel
+        :param reader: asyncio.StreamReader - wrapper for async reading to TCP sockets
+        :param writer: asyncio.StreamWriter - wrapper for async writing to TCP sockets
+        :return:
+        """
+        data = await reader.read(100)  # to run coroutines you need to call them using the 'await' keyword
+        message = data.decode()  # Decoding message from bytestream to utf-8 encoded text
+        addr = writer.get_extra_info("peername")
+        self.connected_clients.append(addr) # Add this client to list of currently connected clients
 
+        # TODO: change to use python logging module rather than print statements
+        print(f"Received {message!r} from {addr!r}")
+        print(f"Send: {message!r}")
 
-# Returns a callback
-async def parse_command(input: str):
-    """
-    Coroutine which parses client commands
-    :param input:
-    :return: action (future)
-    """
-    pass
+        # Need to use write with drain as it might be queued in a write buffer
+        # if it cannot be sent immediately
+        writer.write(data)
+        await writer.drain()
 
+        # print("Close the connection")
+        writer.close()
 
-async def main():
-    # asyncio makes it easy to start tcp servers using start_server
-    # It's already both IPv4 and IPv6
-    server = await asyncio.start_server(handle_client, "localhost", 8888)
+    # Returns a callback
+    async def parse_command(self, input: str):
+        """
+        Coroutine which parses client commands
+        :param input:
+        :return: action (future)
+        """
+        pass
 
-    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
+    async def main(self):
+        # asyncio makes it easy to start tcp servers using start_server
+        # It's already both IPv4 and IPv6
+        server = await asyncio.start_server(self.handle_client, self.hostname, self.port)
 
-    async with server:
-        await server.serve_forever()
+        addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+        print(f"Serving on {addrs}")
+
+        async with server:
+            await server.serve_forever()
 
 
 # Remove debug=True when finished
-asyncio.run(main(), debug=True)
+server = Server()
+asyncio.run(server.main(), debug=True)
