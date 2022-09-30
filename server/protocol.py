39b286a1bd2550d2387e0d596607372258002a84
cruthe93 <cruthe93@gmail.com>
2022-09-30 14:02:00 +0100

    Create protocol.py
    Class file to define the application layer protocol for client-server requests. Includes function code constants and a static method to build json requests

---
+++ server/protocol.py
@@ -0,0 +1,34 @@
+import enum
+
+
+class Protocol(enum.Enum):
+    LOGOUT = 0
+    LOGIN = 1
+    REGISTER = 2
+    READ = 3
+    WRITE = 4
+    LIST = 5
+
+    @staticmethod
+    def build_request(request_type: 'Protocol', to: str, fro: str, payload: str):
+        """
+        Static method to
+        :param request_type: header for function code (see protocol class)
+        :param to: username
+        :param fro: username
+        :param payload: string message
+        :return packet: json representation of packet
+        """
+        packet = {}  # Empty packet
+        match request_type:
+            case Protocol.LOGOUT:
+                pass
+            case Protocol.LOGIN:
+                pass
+            case Protocol.REGISTER:
+                pass
+            case Protocol.READ:
+                packet = {"code": "READ", "fro": fro}
+            case Protocol.WRITE:
+                packet = {"code": "WRITE", "to": to, "payload": payload}
+        return packet
