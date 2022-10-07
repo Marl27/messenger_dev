63db92f8c2064d15672f3eb31313928eb02b2e1a
Marl27 <himalya.sahota@gmail.com>
2022-10-07 12:03:24 +0100

    Added write funtionality to the messenger

--- database/write_chat.py
+++ database/write_chat.py
@@ -1,37 +1,37 @@
-# from .database_connect import db_connect, logging
-# from .config import _insert_register_employee
+from .database_connect import db_connect, logging
+from .config import _insert_private_chat
 
-from database_connect import db_connect, logging
-from config import _insert_register_employee
+# from database_connect import db_connect, logging
+# from config import _insert_private_chat
 
 
 def write_private_chat(sender_id, receiver_id, message, is_broadcasted=False, group_name=None, is_stared=False):
-    # conn, cursor = db_connect()
-    # with conn:
-    #     cursor.execute(_insert_register_employee, ())
-    # logging.info('Employee Registered')
-    print('write_private_chat')
-    print(sender_id, receiver_id, message, is_broadcasted, group_name, is_stared)
+    conn, cursor = db_connect()
+    with conn:
+        cursor.execute(_insert_private_chat, (sender_id, receiver_id, message, is_broadcasted, group_name, is_stared,))
+    logging.info('Private Chat Written')
+    return True
 
 
 def write_group_chat(sender_id, receiver_id, message, is_broadcasted=False, group_name=None, is_stared=False):
-    # conn, cursor = db_connect()
-    # with conn:
-    #     cursor.execute(_insert_register_employee, ())
-    # logging.info('Employee Registered')
-    print('write_group_chat')
-    print(sender_id, receiver_id, message, is_broadcasted, group_name, is_stared)
+    if str(sender_id) not in receiver_id:
+        receiver_id = str(sender_id)+','+receiver_id
+    conn, cursor = db_connect()
+    with conn:
+        cursor.execute(_insert_private_chat, (sender_id, receiver_id, message, is_broadcasted, group_name, is_stared,))
+    logging.info('Group Chat Written')
+    return True
 
 
 def write_chat(sender_id, receiver_id, message, is_broadcasted=False, group_name=None, is_stared=False):
     receiver = list(map(int, receiver_id.split(',')))
     if len(receiver) > 1:
-        logging.info('writing group chat')
-        return write_group_chat(sender_id, receiver, message, is_broadcasted, group_name, is_stared)
+        logging.info('Writing Group Chat')
+        return write_group_chat(sender_id, receiver_id, message, is_broadcasted, group_name, is_stared)
     else:
-        logging.info('writing private chat')
+        logging.info('Writing Private Chat')
         return write_private_chat(sender_id, receiver[0], message, is_broadcasted, group_name, is_stared)
 
-
-if __name__ == "__main__":
-    write_chat(2, '1', 'Hi Charlie', '', None, False)
+#
+# if __name__ == "__main__":
+#     write_chat(2, '2,1,3,4', 'group chat, multiple reciever test1', is_broadcasted=False, group_name=None, is_stared=False)
--- messenger.py
+++ messenger.py
@@ -1,5 +1,5 @@
 from database.read_chat import fetch_chat
-from database.write_chat import fetch_chat
+from database.write_chat import write_chat
 
 
 class Messenger:
@@ -10,4 +10,14 @@ class Messenger:
         self.is_broadcasted = is_broadcasted
         self.group_name = group_name
         self.is_stared = is_stared
+
+    def write_chat_to_messenger(self):
+        result = write_chat(self.sender, self.receiver, self.message, self.is_broadcasted, self.group_name,
+                            self.is_stared)
+        if result:
+            return True
+
+    def read_chat_from_messenger(self):
+        return fetch_chat(self.sender, self.receiver)
+
 
