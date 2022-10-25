d2bd5a9f9e89fdcf8eaf5d56633e77abfcdb6c44
Marl27 <himalya.sahota@gmail.com>
2022-10-25 09:59:31 +0100

    added first version of write_chat

--- tests/unit_tests/database/test_write.py
+++ tests/unit_tests/database/test_write.py
@@ -2,42 +2,33 @@ import sys
 import pytest
 sys.path.append(".")
 from database.write_chat import write_chat
+from database.read_chat import fetch_chat
 from tests.database_config import db_connect_for_testing
 
-
+# TODO - More tests
 expected_chats = {
-    "1": [(1, '2', 0, None, 'private message', 0),
-          (2, '1', 0, None, 'Hi Char', 0),
-          (1, '2', 0, None, 'Hi Him', 0)],
+    "1": [(1, '2', 0, None, 'user_1 sending to user_2', 0)],
 
-    "2": [(1, '2', 0, None, 'private message', 0),
-          (2, '1', 0, None, 'Hi Char', 0),
-          (1, '2', 0, None, 'Hi Him', 0)],
-
-    "2,1,3": [[(1, '1,2,3', 0, 'group_1', 'anyone home?', 0),
-               (2, '1,2,3', 0, 'group_1', 'anyone home?', 0)]],
-
-    "2,1,3,4": [[(1, '1,2,3,4', 0, 'group_2', 'testing sort', 0), (2, '1,2,3,4', 0, 'group_2', 'testing sort 3', 0)],
-                [(2, '2,1,3,4', 0, None, 'group chat, multiple receiver test', 0),
-                 (2, '2,1,3,4', 0, None, 'multiple receiver test1', 0)]],
 }
 
-# TODO
-#
-# @pytest.mark.parametrize("employee_id, receiver_id, expected", [
-#     # Happy Path
-#     (1, '2', expected_chats['2']),
-#     (2, '1', expected_chats['2']),
-#     (1, '2,1,3', expected_chats['2,1,3']),
-#     (2, '2,1,3,4', expected_chats['2,1,3,4']),
-#     # Unhappy Path
-#     # (1, 2, "AttributeError: 'int' object has no attribute 'split'")
-# ])
-# def test_fetch_chat(db_connect_for_testing, employee_id, receiver_id, expected):
-#     conn, cursor = db_connect_for_testing
-#     results = write_chat(conn, cursor, employee_id, receiver_id)
-#     if len(receiver_id) == 1:
-#         results_without_timestamp = [result[:6] for result in results]
-#     else:
-#         results_without_timestamp = [[x[:6] for x in result] for result in results]
-#     assert expected == results_without_timestamp
+
+@pytest.mark.parametrize("sender_id, receiver_id, message, is_broadcasted, group_name, is_stared, expected", [
+                        # Happy Path
+                        (1, '2', 'user_1 sending to user_2', False, None, False, expected_chats['1']),
+])
+def test_write_chat(db_connect_for_testing, sender_id, receiver_id, message,
+                    is_broadcasted, group_name, is_stared, expected):
+    conn, cursor = db_connect_for_testing
+    with conn:
+        cursor.execute('DELETE FROM messenger')
+        write_chat(conn, cursor, sender_id, receiver_id, message, is_broadcasted, group_name, is_stared)
+    results = fetch_chat(conn, cursor, sender_id, receiver_id)
+    # print('**test_write_chat**')
+    # print(results)
+    # print(expected)
+    if len(receiver_id) == 1:
+        results_without_timestamp = [result[:6] for result in results]
+    else:
+        results_without_timestamp = [[x[:6] for x in result] for result in results]
+
+    assert results_without_timestamp == expected
